b632a08cab9a79a53a5122d34324ebeb1c82f9d7
Bhavesh Patil <75352379+bhaveshppatil@users.noreply.github.com>
2021-08-17 23:52:50 +0530

    Add files via upload

--- Kotlin Introduction - YOU/CharacterOccurrence.kt
+++ Kotlin Introduction - YOU/CharacterOccurrence.kt
@@ -13,8 +13,10 @@ fun main() {
         ch[i] = str[i]
         var found = 0
         for (j in 0..i) {
-            if (str[i] == ch[j]) {
-                found++
+            if (str[i] != ' ') {
+                if (str[i] == ch[j]) {
+                    found++
+                }
             }
             if (found == 1) {
                 println(str[i].toString() + "-" + count[str[i].toInt()])

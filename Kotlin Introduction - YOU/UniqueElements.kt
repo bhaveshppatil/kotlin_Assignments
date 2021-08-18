57302405f3ea9942700a13838f5507f4e4d3bbe3
Bhavesh Patil <75352379+bhaveshppatil@users.noreply.github.com>
2021-08-18 10:39:16 +0530

    Add files via upload

--- Kotlin Introduction - YOU/UniqueElements.kt
+++ Kotlin Introduction - YOU/UniqueElements.kt
@@ -1,5 +1,19 @@
 fun main() {
-    var nums = listOf<Int>(10, 20, 30, 10, 40, -2, 30)
+    val arr = intArrayOf(10, 10, 20, 20, -20)
+    var count: Int
+    var temp = false
 
-
+    for (i in arr.indices) {
+        count = 1
+        for (j in arr.indices) {
+            if (arr[i] == arr[j] && i != j) {
+                count++
+            }
+        }
+        if (count == 1) {
+            temp = true
+            print(arr[i].toString() + ", ")
+        }
+    }
+    if (!temp) println("Nothing is unqiue")
 }
